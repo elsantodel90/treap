c82be5815fdc7733140baa7e50f71672c6a93627
Agustin Santiago Gutierrez <elsantodel90@gmail.com>
2015-10-06 12:41:43 -0300

    split y merge, y retoques. Habria que testearrrrrrrr

--- treap.cpp
+++ treap.cpp
@@ -17,6 +17,7 @@ namespace Treap
         
         // *****************************************************************************
         // Estas que vienen aca son las unicas funciones donde se hacen cambios estructurales al arbol, y las que hay que tocar si se aumenta con mas datos.
+        // Normalmente solo hay que agregar un "update" de los nodos cuyos subarboles han cambiando (X y Z en una rotacion por ejemplo)
         
         // Se crea un nuevo nodo sueltito, aislado, raiz de su propio arbol.
         Nodo (const T &d) { dat = d; padre = h[0] = h[1] = NULL;  p = rand(); }
@@ -57,20 +58,32 @@ namespace Treap
             
             h[childIndex ^ 1] = oldDad;
             oldDad->padre = this;
-            
         }
         
-        // Cuelga un hijo de un padre. El hijo antes era una raiz que no tenia padre.
+        // Cuelga un hijo de un padre.
         void hang(int pos, Nodo *child)
         {
             h[pos] = child;
             child->padre = this;
         }
         
+        // Descuelga este hijo de su padre.
+        void chop()
+        {
+            padre->h[padre->h[1] == this] = NULL;
+            padre = NULL;
+        }
+        
         // Fin de funciones que hacen cambios estructurales
         // ************************************************************************
 
-        void flotar() { while (padre && p < padre->p) rotar(); }
+        void flotar() { while (padre && p >= padre->p) rotar(); }
+        
+        void hundir()
+        { 
+            while ((h[0] && h[0]->p > p) || (h[1] && h[1]->p > p))
+                h[h[0] == NULL || (h[1] && h[1]->p > h[0]->p)].rotar(); 
+        }
     };
 
     template <typename T>
@@ -78,14 +91,15 @@ namespace Treap
     {
         Nodo<T> *root;        
         Treap() : root(NULL) {}
-                
+        
+        void reroot() { while (root->padre) root = root->padre; }
+        
         Nodo<T>* lowerBound(const T&x)
         {
             Nodo<T> *res = NULL, *p = root;
             while (p)
             {
-                if (p->dat == x) return p;
-                else if (p->dat >= x) res = p;
+                if (p->dat >= x) res = p;
                 p = p->h[x > p->dat];
             }
             return res;
@@ -96,39 +110,80 @@ namespace Treap
             Nodo<T> *res = NULL, *p = root;
             while (p)
             {
-                if (p->data > x) res = p;
+                if (p->dat > x) res = p;
                 p = p->h[x >= p->dat];
             }
             return res;
         }
-                
-        bool pertenece(const T& x)
+        
+        Nodo<T>* insertionPoint(const T&x)
         {
-            Nodo<T> *p = lowerBound(x);
-            return p && p->dat == x; 
-        }
-
-        Nodo<T> *insertar(const T&x)
-        {
-            if (!root)
+            Nodo<T>* res = root, *prev = NULL;
+            while (res)
             {
-                root = new Nodo<T>(x);
-                return root;
+                prev = res;
+                if (res->dat == x) return res;
+                res = res->h[x > res->dat];
             }
-            Nodo<T> *p = lowerBound(x);
-            if (p && p->dat == x) return p; // Ya estaba
+            return prev;
+        }
+                
+        pair<Nodo<T>*, bool> insertar(const T&x)
+        {
+            if (!root) return make_pair(root = new Nodo<T>(x), true);
+            Nodo<T> *p = insertionPoint(x);
+            if (p->dat == x) return make_pair(p, false);
             Nodo<T> *nu = new Nodo<T>(x);
-            int childIndex = 1;
-            if      (p == NULL)    p = root;
-            else if (p->h[0]  )    p = p->h[0];
-            else                   childIndex = 0;
-            while (p->h[childIndex]) p = p->h[1];
-            p->hang(childIndex, nu);
+            p->hang(x > p->dat, nu);
             nu->flotar();
-            if (nu->padre == NULL) root = nu;
-            return nu;
+            reroot();
+            return make_pair(nu, true);
         }
         
+        void erase(Nodo<T> *p)
+        {
+            p->p = -1;
+            p->hundir();
+            if (p->padre)
+            {
+                reroot();
+                p->chop();
+            }
+            else
+                root = NULL;
+            delete p;
+        }
+        
+        // Toma los datos de this, destruye lo que hubiera en larger
+        // Deja los (-inf, x) en this
+        // Deja los [x, +inf) en larger
+        void split(const T&x, Treap &larger)
+        {
+            pair<Nodo<T> *, bool> par = insertar(x);
+            Nodo<T>* p = par.first;
+            p->p = RAND_MAX;
+            p->flotar();
+            assert(p->padre == NULL);
+            
+            liberar(larger.root);
+            larger.root = p->h[1];
+            root = p->h[0];
+            delete p;
+            if (!par.second)
+                larger.insertar(x);
+        }
+        
+        // Mergea this con larger (Debe ser this < larger en todos sus elementos)
+        // Vacia larger y deja el resultado en this
+        void merge(Treap &larger)
+        {
+            Nodo<T>*p1 = root, *p2 = larger.root;
+            larger.root = NULL;
+            root = new Nodo<T>;
+            root->h[0] = p1;
+            root->h[1] = p2;
+            erase(root);
+        }
         
         static void liberar(Nodo<T> *p)
         {
@@ -145,6 +200,7 @@ namespace Treap
 
 }
 
+#define pertenece(t, x) (t.lowerBound(x) && t.lowerBound(x)->dat == x)
 
 int main()
 {
@@ -152,6 +208,6 @@ int main()
     t.insertar(1);
     t.insertar(2);
     cout << "SSSSSS" << endl;
-    cout << t.pertenece(3) << endl;
+    cout << pertenece(t,0) << endl;
     return 0;
 }
